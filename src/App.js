eeb91357da41d85bf18413c1bfb2899a6dfd9e06
Stefaniya Yanovich <stefanishy@gmail.com>
2021-02-22 13:51:53 +0300

    test2

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { React, useState, useEffect } from "react";
 import './css/style.scss';
 import useSound from 'use-sound';
@@ -66,3 +67,30 @@ function App() {
 }
 
 export default App;
+=======
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+>>>>>>> f95bbb1... Initialize project using Create React App
